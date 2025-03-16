2d79c98db6f4559bd3f220caac6cd61dfc928571
andriykkk <andriykostenkostd@gmail.com>
2025-03-16 11:05:12 +0200

    its moving

---
+++ main.go
@@ -0,0 +1,185 @@
+package main
+
+import (
+	"log"
+	"fmt"
+	"runtime"
+
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.3/glfw"
+	"github.com/go-gl/mathgl/mgl32"
+)
+
+const (
+	windowWidth  = 800
+	windowHeight = 600
+)
+
+var (
+	triangleVertices = []float32{
+		0.0, 0.5, 0.0,  
+		-0.5, -0.5, 0.0, 
+		0.5, -0.5, 0.0, 
+	}
+
+	cameraPos = mgl32.Vec3{0.0, 0.0, 3.0}
+	cameraFront = mgl32.Vec3{0.0, 0.0, -1.0}
+	cameraUp = mgl32.Vec3{0.0, 1.0, 0.0}
+
+	deltaTime = float32(0.0)
+	lastFrame = float32(0.0)
+) 
+
+func init() {
+	runtime.LockOSThread()
+}
+
+func main() {
+	if err := glfw.Init(); err != nil {
+		log.Fatalln("failed to initialize glfw:", err)
+	}
+	defer glfw.Terminate()
+
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+
+	window, err := glfw.CreateWindow(windowWidth, windowHeight, "Cimic", nil, nil)
+	if err != nil {
+		log.Fatalln("failed to create window:", err)
+	}
+	window.MakeContextCurrent()
+	window.SetKeyCallback(keyCallback)
+
+	if err := gl.Init(); err != nil {
+		log.Fatalln("failed to initialize OpenGL:", err)
+	}
+
+	gl.Viewport(0, 0, windowWidth, windowHeight)
+
+	var VBO uint32
+	gl.GenBuffers(1, &VBO)
+	gl.BindBuffer(gl.ARRAY_BUFFER, VBO)
+	gl.BufferData(gl.ARRAY_BUFFER, len(triangleVertices)*4, gl.Ptr(triangleVertices), gl.STATIC_DRAW)
+
+	var VAO uint32
+	gl.GenVertexArrays(1, &VAO)
+	gl.BindVertexArray(VAO)
+
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 3*4, gl.PtrOffset(0))
+	gl.EnableVertexAttribArray(0)
+
+	vertexShaderSource := `
+		#version 410
+		layout (location = 0) in vec3 aPos;
+		uniform mat4 model;
+		uniform mat4 view;
+		uniform mat4 projection;
+		void main() {
+			gl_Position = projection * view * model * vec4(aPos, 1.0);
+		} 
+		` + "\x00"
+
+	fragmentShaderSource := `
+	#version 410
+	out vec4 FragColor;
+	void main() {
+		FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	}
+	` + "\x00"
+
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		log.Fatalln("failed to compile vertex shader:", err)
+	}
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		log.Fatalln("failed to compile fragment shader:", err)
+	}
+
+	shaderProgram := gl.CreateProgram()
+	gl.AttachShader(shaderProgram, vertexShader)
+	gl.AttachShader(shaderProgram, fragmentShader)
+	gl.LinkProgram(shaderProgram)
+	gl.UseProgram(shaderProgram)
+
+	gl.DeleteShader(vertexShader)
+	gl.DeleteShader(fragmentShader)
+
+	projection := mgl32.Perspective(mgl32.DegToRad(45.0), float32(windowWidth)/windowHeight, 0.1, 100.0)
+	projectionUniform := gl.GetUniformLocation(shaderProgram, gl.Str("projection\x00"))
+	gl.UniformMatrix4fv(projectionUniform, 1, false,  &projection[0])
+	
+	model := mgl32.Ident4()
+	modelUniform := gl.GetUniformLocation(shaderProgram, gl.Str("model\x00"))
+	gl.UniformMatrix4fv(modelUniform, 1, false, &model[0])
+
+	for !window.ShouldClose() {
+		currentFrame := float32(glfw.GetTime())
+		deltaTime = currentFrame - lastFrame
+		lastFrame = currentFrame
+
+		processInput(window)
+		
+		gl.ClearColor(0.0, 0.0, 0.0, 1.0)
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+		view := mgl32.LookAtV(cameraPos, cameraPos.Add(cameraFront), cameraUp)
+		viewUniform := gl.GetUniformLocation(shaderProgram, gl.Str("view\x00"))
+		gl.UniformMatrix4fv(viewUniform, 1, false, &view[0])
+		
+		gl.BindVertexArray(VAO)
+		gl.DrawArrays(gl.TRIANGLES, 0, 3)
+
+		window.SwapBuffers()
+		
+		glfw.PollEvents()
+	}
+}
+
+func keyCallback(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
+	if key == glfw.KeyEscape && action == glfw.Press {
+		w.SetShouldClose(true)
+	}
+}
+
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+		log := make([]byte, logLength)
+		gl.GetShaderInfoLog(shader, logLength, nil, &log[0])
+		return 0, fmt.Errorf("shader compilation failed: %s", string(log))
+	}
+
+	return shader, nil
+}
+
+func processInput(window *glfw.Window) {
+	cameraSpeed := float32(2.5) * deltaTime
+
+	if window.GetKey(glfw.KeyW) == glfw.Press {
+		cameraPos = cameraPos.Add(cameraFront.Mul(cameraSpeed))
+	}
+	if window.GetKey(glfw.KeyS) == glfw.Press {
+		cameraPos = cameraPos.Sub(cameraFront.Mul(cameraSpeed))
+	}
+	if window.GetKey(glfw.KeyA) == glfw.Press {
+		cameraPos = cameraPos.Sub(cameraFront.Cross(cameraUp).Normalize().Mul(cameraSpeed))
+	}
+	if window.GetKey(glfw.KeyD) == glfw.Press {
+		cameraPos = cameraPos.Add(cameraFront.Cross(cameraUp).Normalize().Mul(cameraSpeed))
+	}
+	if window.GetKey(glfw.KeySpace) == glfw.Press {
+		cameraPos = cameraPos.Add(cameraUp.Mul(cameraSpeed))
+	}
+	if window.GetKey(glfw.KeyLeftShift) == glfw.Press || window.GetKey(glfw.KeyRightShift) == glfw.Press {
+		cameraPos = cameraPos.Sub(cameraUp.Mul(cameraSpeed))
+	}
+}
